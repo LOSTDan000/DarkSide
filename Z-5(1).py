6fdca232eb19cf66385a3b3bfc394c7f498d709d
LOSTDan000 <114292914+LOSTDan000@users.noreply.github.com>
2022-11-14 18:48:15 +0300

    Update Z-5(1).py

--- Z-5(1).py
+++ Z-5(1).py
@@ -1,1 +1,10 @@
-n = int(input())i = 0a = 0while i * i <= n:    a = i * i    print(a)    i=i+1
+n = 0
+def add(n):
+  n = int(input())
+  i = 0
+  a = 0
+  while i * i <= n:
+      a = i * i
+      print(a)
+      i=i+1
+add(n)
