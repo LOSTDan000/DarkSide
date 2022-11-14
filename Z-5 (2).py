6bd8039303f8169b8f585e19af93b166491a300a
LOSTDan000 <114292914+LOSTDan000@users.noreply.github.com>
2022-11-14 18:49:20 +0300

    Update Z-5 (2).py

--- Z-5 (2).py
+++ Z-5 (2).py
@@ -1,5 +1,8 @@
-N=int(input())
-deli=2
-while N%deli!=0:
-    deli+=1
-print(deli)
+N = 0
+def add(N):
+    N=int(input())
+    deli=2
+    while N%deli!=0:
+        deli+=1
+    print(deli)
+add(N)
